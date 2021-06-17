56c17c8ef76bf3b3c7554161e08580fc1539a4e7
yeuho938 <hothiyeu012345@gmail.com>
2021-06-17 18:24:19 +0700

    Change UI login/register ..., build function login

---
+++ src/app/modules/auth/services/logged-user.service.ts
@@ -0,0 +1,29 @@
+// import {Injectable} from '@angular/core';
+// import {BehaviorSubject, Observable} from 'rxjs';
+
+// import {UserModel} from '@app/modules/core/models';
+
+// @Injectable()
+// export class LoggedUserService {
+//   private _currentUser: BehaviorSubject<UserModel> = new BehaviorSubject(null);
+
+//   constructor() {
+//   }
+
+//   get currentUser(): Observable<UserModel> {
+//     return this._currentUser.asObservable();
+//   }
+
+//   setLoggedUser(user: UserModel) {
+//     this._currentUser.next(user);
+//   }
+
+//   get loggedUserId(): number {
+//     const user = this._currentUser.getValue();
+//     return user ? user.id : null;
+//   }
+
+//   get loggedUser(): UserModel {
+//     return this._currentUser.getValue();
+//   }
+// }
